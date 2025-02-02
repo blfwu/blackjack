867d3ee225c34c2f50a15a235d50f1f3575d772b
Brandon Wu <brandonwuedu@gmail.com>
2025-02-01 22:10:08 -0800

    Version 2: function adds desired # of cards to user or comp, displays card list and player score

--- blackjack.py
+++ blackjack.py
@@ -1,19 +1,25 @@
 import random
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
-player_cards = []
+user_cards = []
 dealer_cards = []
 
-player_score = 0
+user_score = 0
 dealer_score = 0
 
+def random_card(player_cards, player_score, num_cards):
+
+    for i in range(num_cards):
+        player_cards.append(random.choice(cards))
+
+    for i in range(len(player_cards)):
+        player_score += player_cards[i]
+  
+    print(f"Your cards: {player_cards}, current score: {player_score}")
 
 play = input("Do you want to play a game of Blackjack? Type 'y' or 'n': ")
 
 if play == 'y':
-    # PLAYER
-    player_cards.extend([random.choice(cards), random.choice(cards)])
-    player_score = player_cards[0] + player_cards[1]
-    print(f"Your cards: {player_cards}, current score: {player_score}")
 
-    # COMPUTER
+    random_card(user_cards, user_score, 2)
+
